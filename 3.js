c9d7d5253f6f3e386dce65011982f6f2100e28cb
Tessa Thornton <tessathornton@gmail.com>
2013-07-14 18:26:44 -0400

    refactor 3

--- 3.js
+++ 3.js
@@ -2,27 +2,13 @@
 
 What is the largest prime factor of the number 600851475143 ?*/
 
-var product = 600851475143;
-
-function isPrime(n) {
-	for (var i = 2; i < n; i++) {
-		if (n % i === 0) {
-			return false;
-		} 
-	}
-	return true;
-}
-
 function largestPrimeFactor(n) {
 	for (var i = 2; i < n; i++) {
 		if (n % i === 0) {
-			var factor = n/i;
-			if (isPrime(factor)) {
-				console.log(factor); 
-				break;
-			}       
+			n = n/i;			     
 		}
 	}
+	return n;
 }
 
-largestPrimeFactor(product);
+console.log(largestPrimeFactor(600851475143));
